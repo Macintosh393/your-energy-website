fad004418a3e8a05c06095398ee2f7629c2d3e57
Max Kosobutskyi <maxkosobutskyi@gmail.com>
2026-02-03 16:14:44 +0200

    Test paths importing 2

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
-import facebookIcon from './img/socials-icons/facebook.svg?url';
-import instagramIcon from './img/socials-icons/instagram.svg?url';
-import youtubeIcon from './img/socials-icons/youtube.svg?url';
+import facebookIcon from './img/socials-icons/facebook.svg';
+import instagramIcon from './img/socials-icons/instagram.svg';
+import youtubeIcon from './img/socials-icons/youtube.svg';
+
+document.querySelector('#facebook-icon').href = facebookIcon;
+document.querySelector('#instagram-icon').href = instagramIcon;
+document.querySelector('#youtube-icon').href = youtubeIcon;
