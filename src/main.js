67854e6e9d8c77ee992a3e5a53794fc6913049de
Max Kosobutskyi <maxkosobutskyi@gmail.com>
2026-02-03 16:32:25 +0200

    Try different approach to svg paths issue

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import facebookIcon from './img/socials-icons/facebook.svg';
-import instagramIcon from './img/socials-icons/instagram.svg';
-import youtubeIcon from './img/socials-icons/youtube.svg';
-
-document.querySelector('#facebook-icon').href = facebookIcon;
-document.querySelector('#instagram-icon').href = instagramIcon;
-document.querySelector('#youtube-icon').href = youtubeIcon;
