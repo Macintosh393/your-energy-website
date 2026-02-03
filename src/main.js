24f52488e04a93cba0f463fb6977822b5bddc134
Max Kosobutskyi <maxkosobutskyi@gmail.com>
2026-02-03 15:56:51 +0200

    Test paths for icons

---
+++ src/main.js
@@ -0,0 +1,3 @@
+import facebookIcon from './img/socials-icons/facebook.svg';
+import instagramIcon from './img/socials-icons/instagram.svg';
+import youtubeIcon from './img/socials-icons/youtube.svg';
