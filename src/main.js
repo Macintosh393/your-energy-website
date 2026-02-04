2e73e49d798853636840e6715f06c0219a87fee3
Max Kosobutskyi <maxkosobutskyi@gmail.com>
2026-02-04 06:04:13 +0200

    Add pagination

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Pagination from './js/pagination';
+
+const pagination = new Pagination();
+const pagFirstBut = document.querySelector('.pagination-first-button');
+const pagPrevBut = document.querySelector('.pagination-previous-button');
+const pagNextBut = document.querySelector('.pagination-next-button');
+
+pagFirstBut.addEventListener('click', () => pagination.reset());
+pagPrevBut.addEventListener('click', () => pagination.back());
+pagNextBut.addEventListener('click', () => pagination.next());
